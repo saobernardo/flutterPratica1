138ccd9e42a64462f25dc697a8624b25f7b08110
saobernardo <bernardo.pinheiro@hotmail.com>
2021-08-25 13:27:10 -0300

    Jugando con tema

--- lib/main.dart
+++ lib/main.dart
@@ -12,92 +12,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      theme: ThemeData(
+        //brightness: Brightness.dark
+        primaryColor: Colors.red,
+        primaryTextTheme: TextTheme(
+          headline6: TextStyle(fontSize: 20)
+        ),
+        textTheme: TextTheme(bodyText2: TextStyle(color: Colors.blue))
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: Text('Meu primeiro App'),
         ),
         body: Column(
-          children: <Widget>[
-            Image(image: AssetImage('assets/kof.jpg')),
+          children: [
             Container(
               padding: EdgeInsets.all(20),
-              child: Row(
-                children: [
-                  Expanded(
-                    child:Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('KOF \'96' ,style: textStyleName),
-                        Text('Osaka, Japan', style: textStyleLocation)
-                      ]
-                    )
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.star, color: Colors.blue),
-                      Text('9.4')
-                    ],
-
-                  )
-                ]
-              )
-            ),
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Row(
-                children: [
-                  Botao(icon: Icons.call, text: 'Llamar'),
-                  Botao(icon: Icons.location_on, text: 'Dirección'),
-                  Botao(icon: Icons.share, text: 'Compartir')
-                ]
-              )
-            ),
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Text('The King of Fighters nasceu de um crossover entre outras aclamadas séries de luta da época. O jogo pegou emprestado alguns personagens dos sucessos Fatal Fury (de onde vieram os populares Mai Shiranui e Terry Bogard), Art of Fighting (Ryo Sakazaki e Robert Garcia), Ikari Warriors (Ralf Jones, Clark Still e Heidern) e Psycho Soldier (Athena Asamiya, Sie Kensou e Chin Gentsai), e acrescentou outros inéditos, como o protagonista Kyo Kusanagi.')
+              child: Text('Olá mundo'),
             )
-          ]
+          ],
         ),
       )
     );
   } 
-}
-
-class Botao extends StatelessWidget{
-  Botao({
-    required this.icon,
-    required this.text
-  });
-  
-  final IconData icon;
-  final text;
-
-  onPress(){
-    switch(text){
-      case 'Llamar':
-        print('Botón llamar');
-      break;
-      case 'Dirección':
-        print('Botón Dirección');
-      break;
-      case 'Compartir':
-        print('Botón compartir');
-      break;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context){
-    return Expanded(
-            child: TextButton(
-              onPressed: onPress, 
-              child: Column(
-                children: [
-                  Icon(icon, color: Colors.blue),
-                  Text(text)
-                ],
-              )
-            )
-          );
-  }
 }
